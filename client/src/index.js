a346d6516f00517db88f1e4cfc6884ac1cc3b870
Francisco Gramajo <frangrama510@gmail.com>
2022-09-07 21:18:40 -0300

    adding routes

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./views/Home";
+import App from "./views/App";
 import { store } from "./state/store";
 import { Provider } from "react-redux";
 import { ThemeProvider } from "@mui/material";
 import theme from "./config/theme";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <ThemeProvider theme={theme}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ThemeProvider>
   </Provider>
 );
