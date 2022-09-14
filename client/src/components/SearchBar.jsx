bc6d9ab8b9e54fef7e6c6f22f80e6a48dcfd20e8
Francisco Gramajo <frangrama510@gmail.com>
2022-09-14 16:38:06 -0300

    adding a new searching modal

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,60 +1,36 @@
 import * as React from "react";
-import { styled } from "@mui/material/styles";
-import InputBase from "@mui/material/InputBase";
-import VisibilityIcon from "@mui/icons-material/Visibility";
 
-export const SearchBar = () => {
-  const Search = styled("div")(({ theme }) => ({
-    position: "relative",
-    borderRadius: "25px",
-    backgroundColor: "#2b2620",
-    "&:hover": {
-      backgroundColor: "#3c3832",
-    },
-    marginLeft: 0,
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      marginLeft: theme.spacing(1),
-      width: "auto",
-    },
-  }));
+import PageviewIcon from "@mui/icons-material/Pageview";
+import { Box, IconButton } from "@mui/material";
+import { ModalSearching } from "./ModalSearching";
+import { useState } from "react";
 
-  const SearchIconWrapper = styled("div")(({ theme }) => ({
-    padding: theme.spacing(0, 2),
-    height: "100%",
-    position: "absolute",
-    pointerEvents: "none",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-  }));
-
-  const StyledInputBase = styled(InputBase)(({ theme }) => ({
-    color: "inherit",
-    "& .MuiInputBase-input": {
-      padding: theme.spacing(1, 1, 1, 0),
-      // vertical padding + font size from searchIcon
-      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-      transition: theme.transitions.create("width"),
-      width: "100%",
-      [theme.breakpoints.up("sm")]: {
-        width: "12ch",
-        "&:focus": {
-          width: "40ch",
-        },
-      },
-    },
-  }));
+export const SearchBar = (open, onClose) => {
+  const [dialog, setDialog] = useState(false);
 
   return (
-    <Search>
-      <SearchIconWrapper>
-        <VisibilityIcon />
-      </SearchIconWrapper>
-      <StyledInputBase
-        placeholder="Search here"
-        inputProps={{ "aria-label": "search" }}
+    <>
+      <IconButton onClick={() => setDialog(true)}>
+        <Box
+          sx={{
+            height: "100%",
+            position: "absolute",
+            pointerEvents: "none",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            color: "white",
+          }}
+        >
+          <PageviewIcon fontSize="large" />
+        </Box>
+      </IconButton>
+      <ModalSearching
+        open={dialog}
+        onClose={() => {
+          setDialog(false);
+        }}
       />
-    </Search>
+    </>
   );
 };
