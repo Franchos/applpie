4e4405c808ab4dcf9073dd2681c0b6a8ae8e4e5a
Francisco Gramajo <frangrama510@gmail.com>
2022-09-14 16:22:40 -0300

    refactor trending carousel

--- client/src/components/TrendingCarousel.jsx
+++ client/src/components/TrendingCarousel.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
-import Slider from "react-slick";
+
 import { Box, Typography } from "@mui/material";
 import axios from "axios";
-import MovieCard from "../common/Card";
+import MovieCard from "../common/MovieCard";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 import { TrendingSettings } from "../helpers/CarouselSettings";
 
@@ -18,30 +19,20 @@ const TrendingCarousel = () => {
   }, []);
 
   return (
-    <>
-      <Box
-        align="justify"
-        sx={{
-          m: "10vh 2.8vh 2.8vh",
-        }}
-      >
+    <Box sx={{ minHeight: "0px", minWidth: "0px", m: "10vw" }}>
+      <Box sx={{ alignItems: "center" }}>
         <Typography variant="h6" align="start">
           Fresh from the oven
         </Typography>
-        <Slider {...TrendingSettings} className="content">
-          {movies.results?.map((e, i) => (
-            <MovieCard
-              key={i}
-              title={
-                e.title.length > 20 ? e.title.substring(0, 20) + "..." : e.title
-              }
-              poster_path={e.poster_path}
-              id={e.id}
-            />
-          ))}
-        </Slider>
+        <Box sx={{ p: "5vh" }}>
+          <Slider {...TrendingSettings} className="content">
+            {movies.results?.map((e, i) => (
+              <MovieCard key={i} movie={e} />
+            ))}
+          </Slider>
+        </Box>
       </Box>
-    </>
+    </Box>
   );
 };
 
