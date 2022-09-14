f03953f362f3b3aa0de6cbedd6ad534cc1e4b3c9
Francisco Gramajo <frangrama510@gmail.com>
2022-09-14 16:39:48 -0300

    refactor theme material ui

--- client/src/config/theme.jsx
+++ client/src/config/theme.jsx
@@ -1,12 +1,17 @@
 import { createTheme } from "@mui/material";
-import { grey } from "@mui/material/colors";
 
 const theme = createTheme({
   palette: {
-    primary: { main: "#0F0C08" },
-    secondary: {
-      main: "#e91e63",
+    primary: { main: "#0F0C08", second: "#ffffff" },
+    secondary: { main: "#35363a", second: "#242424", third: "#e0e1e1" },
+    third: {
+      main: "#ffffff",
     },
+  },
+  typography: {
+    title: { fontSize: "h3", color: "#060606" },
+    subtitle1: { fontSize: "h6", color: "#060606" },
+    body1: { fontSize: "1rem", color: "#060606" },
   },
 });
 
