cdfa15362982ce6af36632c6c00301a160f5a9be
Francisco Gramajo <frangrama510@gmail.com>
2022-09-14 16:37:36 -0300

    refactor navbars

--- client/src/components/HomeNavbar.jsx
+++ client/src/components/HomeNavbar.jsx
@@ -1,5 +1,8 @@
 import * as React from "react";
 import { AccountCircle } from "@mui/icons-material";
+import Person4RoundedIcon from "@mui/icons-material/Person4Rounded";
+import PendingRoundedIcon from "@mui/icons-material/PendingRounded";
+import DoorFrontRoundedIcon from "@mui/icons-material/DoorFrontRounded";
 import {
   Button,
   Typography,
@@ -8,11 +11,13 @@ import {
   Box,
   AppBar,
 } from "@mui/material";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
 
 import { SearchBar } from "./SearchBar";
 import { useSelector } from "react-redux";
+import { ModalSearching } from "./ModalSearching";
+import { useEffect } from "react";
 
 const styles = {
   transparentNavbarStyle: {
@@ -46,10 +51,14 @@ const styles = {
 
 export function HomeNavbar() {
   const user = useSelector((state) => state.user);
+  const navigate = useNavigate();
+  const [dialog, setDialog] = React.useState(false);
   const [eventListener, setEventListener] = useState(true);
   const [navBackground, setNavBackground] = useState(
     styles.transparentNavbarStyle
   );
+
+  // console.log(user);
 
   const changeNavbarStyle = () => {
     if (window.scrollY >= 1090) {
@@ -64,55 +73,80 @@ export function HomeNavbar() {
   window.addEventListener("scroll", changeNavbarStyle);
   window.removeEventListener("scroll", changeNavbarStyle, eventListener);
 
+  const handleLogout = () => {
+    axios.post("/auth/logout");
+  };
+
   return (
-    <AppBar position={"fixed"} style={navBackground}>
-      <Toolbar style={styles.toolbarStyle}>
-        <Typography style={styles.titleStyle} variant="h6" component="div">
-          IDKWTW
-        </Typography>
-        {eventListener ? (
-          <Box style={styles.transparentBox}></Box>
-        ) : (
-          <SearchBar />
-        )}
-        {!user._id && (
-          <Box>
-            <Link style={{ textDecoration: "none" }} to="/login">
-              <Button
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                style={styles.buttonStyle}
-              >
-                Login
-              </Button>
-            </Link>
-            <Link style={{ textDecoration: "none" }} to="/register">
-              <Button
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                style={styles.buttonStyle}
-              >
-                Register
-              </Button>
-            </Link>
-          </Box>
-        )}
-        {user._id && (
-          <IconButton
-            size="large"
-            aria-label="account of current user"
-            aria-controls="menu-appbar"
-            aria-haspopup="true"
-            color="inherit"
+    <>
+      <AppBar position={"fixed"} style={navBackground}>
+        <Toolbar style={styles.toolbarStyle}>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
           >
-            <AccountCircle />
-          </IconButton>
-        )}
-      </Toolbar>
-    </AppBar>
+            <Typography style={styles.titleStyle} variant="h6" component="div">
+              IDKWTW
+            </Typography>
+            <PendingRoundedIcon color="white" size="large" />
+          </Box>
+
+          {eventListener ? <Box></Box> : <SearchBar />}
+          {!user._id && (
+            <Box>
+              <Link style={{ textDecoration: "none" }} to="/login">
+                <Button
+                  size="large"
+                  aria-label="account of current user"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  style={styles.buttonStyle}
+                >
+                  Login
+                </Button>
+              </Link>
+              <Link style={{ textDecoration: "none" }} to="/register">
+                <Button
+                  size="large"
+                  aria-label="account of current user"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  style={styles.buttonStyle}
+                >
+                  Register
+                </Button>
+              </Link>
+            </Box>
+          )}
+          {user._id && (
+            <Box>
+              <IconButton
+                // size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                color="inherit"
+                onClick={() => navigate(`/user/${user._id}`)}
+              >
+                <Person4RoundedIcon fontSize="medium" />
+              </IconButton>
+              <IconButton
+                // size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                color="inherit"
+              >
+                <DoorFrontRoundedIcon fontSize="medium" />
+              </IconButton>
+            </Box>
+          )}
+        </Toolbar>
+      </AppBar>
+      {/* modal */}
+    </>
   );
 }
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,6 +2,10 @@ import * as React from "react";
 
 import MenuIcon from "@mui/icons-material/Menu";
 import { AccountCircle } from "@mui/icons-material";
+import Person4RoundedIcon from "@mui/icons-material/Person4Rounded";
+import PendingRoundedIcon from "@mui/icons-material/PendingRounded";
+import DoorFrontRoundedIcon from "@mui/icons-material/DoorFrontRounded";
+import CottageIcon from "@mui/icons-material/Cottage";
 
 import {
   Button,
@@ -14,60 +18,100 @@ import {
 
 import { useSelector } from "react-redux";
 import { SearchBar } from "./SearchBar";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const styles = {
   navbarStyle: {
     display: "flex",
     justifyContent: "space-around",
     width: "100vw",
+    boxShadow: "0 0 0 0",
   },
   toolbarStyle: {
     display: "flex",
     justifyContent: "space-around",
   },
   buttonStyle: {
-    color: "#e0e1e1",
-    backgroundColor: "grey",
+    color: "black",
+    backgroundColor: "white",
+    margin: "5px",
   },
 };
 
 export function MenuAppBar() {
   const user = useSelector((state) => state.user);
+  const navigate = useNavigate();
 
   return (
     <AppBar position="fixed" style={styles.navbarStyle}>
       <Toolbar style={styles.toolbarStyle}>
-        <Typography style={styles.titleStyle} variant="h6" component="div">
-          IDKWTW
-        </Typography>
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <Typography
+            onClick={() => navigate("/")}
+            style={styles.titleStyle}
+            variant="h6"
+            component="div"
+          >
+            IDKWTW
+          </Typography>
+          <PendingRoundedIcon color="white" size="large" />
+        </Box>
         <SearchBar />
-        {!user._id && (
-          <>
-            <Link to="/login">
-              <Button
-                size="large"
+        <Box>
+          {!user._id && (
+            <>
+              <Link style={{ textDecoration: "none" }} to="/login">
+                <Button
+                  size="large"
+                  aria-label="account of current user"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  style={styles.buttonStyle}
+                >
+                  Login
+                </Button>
+              </Link>
+            </>
+          )}
+          {user._id && (
+            <Box>
+              <IconButton
                 aria-label="account of current user"
                 aria-controls="menu-appbar"
                 aria-haspopup="true"
-                style={styles.buttonStyle}
+                color="inherit"
+                onClick={() => navigate(`/`)}
               >
-                Login
-              </Button>
-            </Link>
-          </>
-        )}
-        {user._id && (
-          <IconButton
-            size="large"
-            aria-label="account of current user"
-            aria-controls="menu-appbar"
-            aria-haspopup="true"
-            color="inherit"
-          >
-            <AccountCircle />
-          </IconButton>
-        )}
+                <CottageIcon />
+              </IconButton>
+              <IconButton
+                // size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                color="inherit"
+                onClick={() => navigate(`/user/${user._id}`)}
+              >
+                <Person4RoundedIcon fontSize="medium" />
+              </IconButton>
+              <IconButton
+                // size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                color="inherit"
+              >
+                <DoorFrontRoundedIcon fontSize="medium" />
+              </IconButton>
+            </Box>
+          )}
+        </Box>
       </Toolbar>
     </AppBar>
   );
