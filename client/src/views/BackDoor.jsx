0b5552174a35fc92e4226311bf6e0a7a96839612
Francisco Gramajo <frangrama510@gmail.com>
2022-09-14 16:24:53 -0300

    refactor outlet and view

--- client/src/views/BackDoor.jsx
+++ client/src/views/BackDoor.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Outlet } from "react-router-dom";
-
+import { useDispatch } from "react-redux";
+import axios from "axios";
+import { setUser } from "../state/user";
 import { BackNavbar } from "../components/BackNavbar";
 
 export const BackDoor = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    axios.get("/auth/me").then(({ data }) => dispatch(setUser(data)));
+  }, [dispatch]);
+
   return (
     <>
       <BackNavbar />
--- client/src/views/FrontDoor.jsx
+++ client/src/views/FrontDoor.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Outlet } from "react-router-dom";
-
+import { useDispatch } from "react-redux";
+import axios from "axios";
+import { setUser } from "../state/user";
 import { HomeNavbar } from "../components/HomeNavbar";
 
 export const WithoutNavBar = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    axios.get("/auth/me").then(({ data }) => dispatch(setUser(data)));
+  }, [dispatch]);
+
   return (
     <>
       <HomeNavbar />
