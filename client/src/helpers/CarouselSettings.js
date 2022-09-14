79d1c5b63f2f65b126b61a0d8a00a5caf7218ff1
Francisco Gramajo <frangrama510@gmail.com>
2022-09-14 16:28:35 -0300

    adding favs and recommended settings

--- client/src/helpers/CarouselSettings.js
+++ client/src/helpers/CarouselSettings.js
@@ -32,9 +32,40 @@ export const GenresSettings = {
   className: "center",
   centerMode: true,
   infinite: true,
-  centerPadding: "60px",
+  centerPadding: "50px",
+  touchDrag: false,
   slidesToShow: 3,
-  speed: 500,
+  draggable: true,
+  speed: 700,
+  swipeToSlide: true,
+  touchThreshold: 100,
+  nextArrow: <ArrowForwardIosIcon sx={{ color: "white" }} />,
+  prevArrow: <ArrowBackIosNewIcon sx={{ color: "white" }} />,
+};
+
+export const FavsSettings = {
+  zoomFactor: 50,
+  dots: false,
+  infinite: true,
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  autoplay: false,
+  speed: 700,
+  rows: 1,
+  nextArrow: <ArrowForwardIosIcon sx={{ color: "white" }} />,
+  prevArrow: <ArrowBackIosNewIcon sx={{ color: "white" }} />,
+};
+
+export const RecommendedSettings = {
+  zoomFactor: 50,
+  dots: false,
+  infinite: true,
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3489,
+  pauseOnHover: true,
+  rows: 2,
   nextArrow: <ArrowForwardIosIcon sx={{ color: "white" }} />,
   prevArrow: <ArrowBackIosNewIcon sx={{ color: "white" }} />,
 };
