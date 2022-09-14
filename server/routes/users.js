9a049e13735fd6696a58b9e86cdcd7ead52f6010
Francisco Gramajo <frangrama510@gmail.com>
2022-09-14 16:06:02 -0300

    adding favs to users routes

--- server/routes/users.js
+++ server/routes/users.js
@@ -9,4 +9,11 @@ router.put("/:_id", validateAuth, UserController.putUser);
 
 router.delete("/:_id", validateAuth, UserController.deleteUser);
 
+//favorites
+router.post("/fav", UserController.setFav);
+
+// router.put("/fav/:user_id", UserController.getFavs);
+
+router.put("/fav/:user_id", UserController.removeFav);
+
 module.exports = router;
