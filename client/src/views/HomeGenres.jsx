2563843b209c8a827f6698029a72eaadc898325f
Francisco Gramajo <frangrama510@gmail.com>
2022-09-14 16:14:21 -0300

    adding a new home view

--- client/src/views/HomeGenres.jsx
+++ client/src/views/HomeGenres.jsx
@@ -1,36 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Box } from "@mui/material";
 
 import { GenresCarousel } from "../components/GenresCarousel";
 import TrendingCarousel from "../components/TrendingCarousel";
-import BasicCarousel from "../components/BasicCarousel";
-
-// import TrendingGrid from "../components/TrendingGrid";
+import { useDispatch } from "react-redux";
+import axios from "axios";
+import { setUser } from "../state/user";
+import { TopTenGrid } from "../components/TopTenGrid";
 
 const styles = {
   backgroundBox: {
-    height: "1200px",
-    width: "100%",
-
-    //ARREGLAR EL LADO DERECHO.........
-
+    height: "120vh",
     display: "flex",
     backgroundSize: "cover",
     backgroundPosition: "center",
     justifyContent: "center",
     alignItems: "center",
     margin: 0,
+    overflowX: "hidden",
   },
 };
 
 export const HomeGenres = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    axios.get("/auth/me").then(({ data }) => dispatch(setUser(data)));
+  }, [dispatch]);
+
   return (
     <>
       <Box backgroundColor="primary.main" style={styles.backgroundBox}>
         <GenresCarousel />
       </Box>
+      <TopTenGrid />
       <TrendingCarousel />
-      <BasicCarousel />
     </>
   );
 };
