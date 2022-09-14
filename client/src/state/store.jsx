7b9046888ff4aada3d5e4e0d47527ff2b81160bc
Francisco Gramajo <frangrama510@gmail.com>
2022-09-14 16:38:22 -0300

    refactor store

--- client/src/state/store.jsx
+++ client/src/state/store.jsx
@@ -2,14 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import logger from "redux-logger";
 import { userReducer } from "./user";
-import { moviesReducer } from "./movies";
 import { favsReducer } from "./favs";
 
 export const store = configureStore({
   middleware: (mw) => mw().concat(logger),
   reducer: {
     user: userReducer,
-    // movies: moviesReducer,
     favs: favsReducer,
   },
 });
