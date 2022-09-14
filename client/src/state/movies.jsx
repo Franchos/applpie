a30dc73f53fc4f2842b7151942e7f25da22d5f97
Francisco Gramajo <frangrama510@gmail.com>
2022-09-14 16:38:45 -0300

    bye movies

--- client/src/state/movies.jsx
+++
@@ -1,7 +0,0 @@
-import { createAction, createReducer } from "@reduxjs/toolkit";
-
-export const getMovies = createAction("GET_MOVIES");
-
-export const moviesReducer = createReducer([], {
-  [getMovies]: (state, action) => action.payload,
-});
